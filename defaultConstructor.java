ff0c27b89bd63afd6aa2a60bc5d35283237eb260
unknown <asimbagwan>
2026-05-26 22:44:47 +0530

    another push

--- defaultConstructor.java
+++ defaultConstructor.java
@@ -1,20 +1,18 @@
 public class defaultConstructor {
+    String Employ_name;
+    float salary;
+   
+    
+    void show(){
+         System.out.println("The employe name is"+Employ_name);
+        System.out.println("the salary is "+salary);
 
-    String brand;
-    int Speed;
-     defaultConstructor(){
-        System.out.print("Bike created");
-     }
-     void show(){
-        System.out.println("The Speed is "+Speed);
-        System.out.print("The brand is " +brand);
-
-     }
-     public static void main(String[] args) {
-        defaultConstructor obj=new defaultConstructor();
-        obj.brand="honda";
-        obj.Speed= 2000;
-        obj.show();
-     }
+    }
+    public static void main(String[] args) {
+        defaultConstructor em1=new defaultConstructor();
+        em1.Employ_name="Asim";
+        em1.salary=52000;
+        em1.show();
+    }
     
 }
